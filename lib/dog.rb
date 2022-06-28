c2b4b1423c07346bc6bdb799229578e8d50a4f84
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-27 18:21:39 -0700

    commit tree 46b78eb8dd6794de1fefc0cd449def07d5256ec7

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+    def bark
+    puts "Woof!"
+    end 
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+
+bruno = Dog.new
+bruno.bark
+
+snoopy = Dog.new
+snoopy.bark
+
+
+
